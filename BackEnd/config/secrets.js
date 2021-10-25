950e6c0845793a114c5ffa815b1991a64487b39e
rajivrtk11 <rajivrtk11@gmail.com>
2021-10-25 10:19:20 +0530

    added

---
+++ BackEnd/config/secrets.js
@@ -0,0 +1,3 @@
+module.exports = {
+    DB_LINK : "mongodb+srv://rajivrtk12:7ncnXESijc-S9kK@cluster0.uuwtw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
---
+++ config/secrets.js
@@ -0,0 +1,3 @@
+module.exports.SECRET_KEY = "1234556";
+module.exports.GMAIL_ID = "rajivrtk111@gmail.com";
+module.exports.GMAIL_PW = "12345678";
